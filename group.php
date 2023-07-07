a7c4306d0d7c0b8427444ba654ae1f295eb8f4fa
Nathnael Damte <124942505+Ndamte@users.noreply.github.com>
2023-07-07 16:23:49 -0400

    Add files via upload

---
+++ group.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Submit Form Twice</title>
+    <link rel="stylesheet" href="group_style.css">
+</head>
+<body>
+<?php
+   
+
+    // Define variables to store group information
+    $group1Name = $group1Err = "";
+    $group2Name = $group2Err = "";
+    
+    // Check if form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+           if(empty($_POST["group1Name"])){
+             $group1Err = "*name is required";
+           }elseif (preg_match("/^[A-Za-z0-9_ ]+$/",$group1Name)) {
+            $group1Err = "*Only letters,numbers and underscore are allowed.";
+           }else{
+                 $group1Name = $_POST["group1Name"];
+                 $_SESSION["group1Name"] = $group1Name;
+           }
+
+
+           if(empty($_POST["group2Name"])){
+            $group2Err = "*name is required";
+          }elseif (preg_match("/^[A-Za-z0-9_ ]+$/",$group2Name)) {
+           $group2Err = "*Only letters,numbers and underscore are allowed.";
+          }else{
+                $group2Name = $_POST["group2Name"];
+                $_SESSION["group2Name"] = $group2Name;
+          }
+         
+          if (empty($group1Err) && empty($group2Err) ) {
+            // No errors, proceed with signup process
+            header("Location: mainBoard.php");
+            exit;
+        }
+
+     }
+?>
+  <div class= "main_class" >
+    
+    <div class= "team_header">
+    <h2>Group 1</h2>
+    </div>
+    
+    <form method="post" action="group.php">
+        <div class= "group_fields">
+        <div class="g1name_n_span">
+  
+        <label class="label">Name:</label>
+        <input class="input" type="text" name="group1Name" required>
+        <span class= "error">  <?php echo $group1Err; ?> </span> <br>
+    </div>
+      
+     <div class="center_header team_header">
+        <h2> Group 2 </h2>
+    </div>
+    
+        <div class="g2name_n_span">
+        <label class="label" >Name:</label>
+        <input class="input" type="text" name="group2Name" required>
+        <span class= "error">  <?php echo $group2Err; ?> </span> <br>
+    </div>
+    <div class= "submit_align">
+        <input type="submit" name="G2submit" value="Set up Teams">
+    </div>
+       </form>
+
+   
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,123 @@
+<?php
+ session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jeopardy</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="signup_styles.css">
+    <!-- Additional meta tags, CSS links, or JavaScript links can be added here -->
+</head>
+<body>
+    <?php
+	
+	 $username= $usernameErr = "";
+	 $email = $emailErr ="";
+	 $password= $passwordErr= "";
+
+	 
+	
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			
+			
+			if (empty($_POST["username"])) {
+               $usernameErr = "*Name is required"; 
+			
+            }elseif (!preg_match("/^[A-Za-z_]\w*$/",$username)) {
+					$usernameErr = "*Only letters,numbers and underscore are allowed. Can not start with a number.";
+			
+			}else{
+				$username = $_POST["username"];
+				$_SESSION["username"] = $username;
+			}
+
+			 
+
+
+			if (empty($_POST["email"])) {
+				$emailErr = "*Email is required";
+				
+			} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "*Invalid email format";
+				
+			}else{
+				$email = $_POST["email"];
+				$_SESSION["email"] = $email;
+
+			}
+			
+			
+			if (empty($_POST["password"])) {
+               $password = "*password is required";
+			  
+            }elseif (!preg_match("/^[A-Za-z_0-9$][A-Za-z0-9_#*$]*$/",$password)) {
+					$passwordErr = "*Only letters,numbers and underscore are allowed.";
+					
+			} else{
+				
+				 	$password = $_POST["password"];
+					$_SESSION["password"] = $password;
+
+			      }
+
+			
+			
+			if (empty($usernameErr) && empty($emailErr) && empty($passwordErr)) {
+				// No errors, proceed with signup process
+				header("Location: login.php");
+				exit;
+			}
+			 
+			
+        }
+	?>
+	 <div class="main_class"> 
+     <div class="center_header">
+	 <div class="signup_header"> 
+		<p> Sign up <p>
+	</div>
+	</div>
+	<form class="form" method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> ">
+   
+	<div class = "signup_fields">
+	<div class ="usr_n_span">
+	
+		
+	  <label> username </label>
+		
+      <input type="text" name="username" required> 
+	
+	 <span class= "error">  <?php echo $usernameErr; ?> </span> <br>
+	</div>
+	<div class ="eml_n_span">
+	
+
+	<label > Email  </label>
+
+	 <input type="text" name="email" > 
+	
+  		<span class= "error">  <?php echo $emailErr; ?> </span> <br>
+	</div>
+	<div class ="pass_n_span">
+	
+		
+	<label > password  </label>
+		 <input type="password" name="password"  required>
+	
+	
+	  <span class= "error">  <?php echo $passwordErr; ?> </span> <br>
+	</div>
+	<div class= "submit_align">
+	 <input type= "submit"><br>
+    
+     </div>
+	 </div>
+
+	
+	</form>
+	</div>
+	 
+</body>
+</html>
+

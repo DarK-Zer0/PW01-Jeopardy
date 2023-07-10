d97067e9f5a20e9bf171f0e2b41ad6b6560b64aa
Indra Meyers <67623845+DarK-Zer0@users.noreply.github.com>
2023-07-09 20:59:32 -0400

    Tidied up code for easier readability

--- login.php
+++ login.php
@@ -1,90 +1,82 @@
 <?php
- session_start();
+     session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Login - Jeopardy</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="login_style.css">
-    <!-- Additional meta tags, CSS links, or JavaScript links can be added here -->
-</head>
-<body>
-     <?php
-	  
-	    $username= $usernameErr = "";
-	   $password= $passwordErr= "";
-       $usr_n_passErr = "";
+     <head>
+          <title>Login - Jeopardy</title>
+          <meta charset="UTF-8">
+          <link rel="stylesheet" href="login_style.css">
+     </head>
+     <body>
+          <?php
+               // Error Messages
+               $username= $usernameErr = "";
+               $password= $passwordErr= "";
+               $usr_n_passErr = "";
 
-       // Pre-made accounts
-       $hc_users = array(
-          "Indra"=>"zer0",
-          "Marcus"=>"pass2",
-          "Nathnael"=>"pass3",
-          "Zian"=>"pass4"
-       );
-	
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			
-			
-			if (empty($_POST["username"])) {
-               $usernameErr = "Name is required";
-			   
-            } 
-              if (!preg_match("/^[A-Za-z_]\w*$/",$_POST['username'])) {
-                $usernameErr = "*Only letters,numbers and underscore are allowed. Can not start with a number.";
-              } else {
-               $username = $_POST["username"];
-               }
-			 
-			if (empty($_POST["password"])) {
-               $password = "password is required";
-            } 
+               // Pre-made accounts
+               $hc_users = array(
+                    "Indra"=>"zer0",
+                    "Marcus"=>"pass2",
+                    "Nathnael"=>"pass3",
+                    "Zian"=>"pass4"
+               );
 
-             if (!preg_match("/^[A-Za-z_0-9$][A-Za-z0-9_#*$]*$/",$_POST['password'])) {
-				$passwordErr = "*wrong password pattern.";
-			}else {
-				$password = $_POST["password"];
-             }
+               if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (empty($_POST["username"])) {
+                         $usernameErr = "Name is required";
+                    } 
+                    if (!preg_match("/^[A-Za-z_]\w*$/",$_POST['username'])) {
+                         $usernameErr = "*Only letters,numbers and underscore are allowed. Can not start with a number.";
+                    } else {
+                         $username = $_POST["username"];
+                    }
 
-             if (isset($hc_users[$username]) && $hc_users[$username] === $password) {
-                header("Location: group.php");
-                exit;
-           } else {
-               $usr_n_passErr= "*Incorrect username or password";
-            }
-		}	 
-	 
-	 ?>
-      <div class="pic_header"> <img src="./img/jlogo.jpg"> </img>  </div>
-      <h1 class= "login_header"> Login </h1>
-	 <form  method="post" action= " <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> ">
-	 <div class="login_fields">
-  
-     <div class="usr_n_span">
-    
-       <label class="label" for="u_name"> Username 
-     <input class="input" type="text" id="u_name" name="username" required> 
-     </label> 
-	  <span class= "error">  <?php echo $usernameErr; ?> </span> <br>
-    </div>
-    
-     <div class="pass_n_span">
-    
-	<label class="label" for="pass"> Password 
-     <input class="input" type="password" id="pass" name="password"  required >
-     </label> 
-	 <span class= "error">  <?php echo $passwordErr; ?> </span> <br>
-    </div>
+                    if (empty($_POST["password"])) {
+                         $password = "password is required";
+                    } 
 
-     <div class= "submit_align">
-	 <input type= "submit"><br>
-    
-     </div>
-     <div class="mistake"> 
-     <span> <?php echo $usr_n_passErr; ?> </span> <br>
-    </div>
-	 </div>
-	</form>
-</body>
+                    if (!preg_match("/^[A-Za-z_0-9$][A-Za-z0-9_#*$]*$/",$_POST['password'])) {
+                         $passwordErr = "*wrong password pattern.";
+                    } else {
+                         $password = $_POST["password"];
+                    }
+
+                    if (isset($hc_users[$username]) && $hc_users[$username] === $password) {
+                         header("Location: group.php");
+                         exit;
+                    } else {
+                         $usr_n_passErr= "*Incorrect username or password";
+                    }
+               }	 
+          ?>
+          <div class="pic_header"> <img src="./img/jlogo.jpg"> </div>
+          <h1 class= "login_header"> Login </h1>
+          <form  method="post" action= " <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> ">
+               <div class="login_fields">
+                    <div class="usr_n_span">
+                         <label class="label" for="u_name"> Username 
+                              <input class="input" type="text" id="u_name" name="username" required> 
+                         </label> 
+                         <span class= "error">  <?php echo $usernameErr; ?> </span> <br>
+                    </div>
+
+                    <div class="pass_n_span">
+                         <label class="label" for="pass"> Password 
+                              <input class="input" type="password" id="pass" name="password"  required >
+                         </label> 
+                         <span class= "error">  <?php echo $passwordErr; ?> </span> <br>
+                    </div>
+
+                    <div class= "submit_align">
+                         <input type= "submit"><br>
+                    </div>
+
+                    <div class="mistake"> 
+                         <span> <?php echo $usr_n_passErr; ?> </span> <br>
+                    </div>
+               </div>
+          </form>
+     </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,115 +1,93 @@
 <?php
- session_start();
+	session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Signup - Jeopardy</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="signup_styles.css">
-    <!-- Additional meta tags, CSS links, or JavaScript links can be added here -->
-</head>
-<body>
-    <?php
-	
-	 $username= $usernameErr = "";
-	 $email = $emailErr ="";
-	 $password= $passwordErr= "";
+	<head>
+		<title>Signup - Jeopardy</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="signup_styles.css">
+	</head>
+	<body>
+		<?php
+			$username= $usernameErr = "";
+			$email = $emailErr ="";
+			$password= $passwordErr= "";
 
-	 
-	
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			
-			
-			if (empty($_POST["username"])) {
-               $usernameErr = "*Name is required"; 
-			
-            }elseif (!preg_match("/^[A-Za-z_][A-Za-z0-9_]*$/",$_POST["username"])) {
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				// Username
+				if (empty($_POST["username"])) {
+					$usernameErr = "*Name is required";
+				} else if (!preg_match("/^[A-Za-z_][A-Za-z0-9_]*$/",$_POST["username"])) {
 					$usernameErr = "*Only letters,numbers and underscore are allowed. Can not start with a number.";
-			
-			}else{
-				$username = $_POST["username"];
-				$_SESSION["username"] = $username;
-			}
+				} else {
+					$username = $_POST["username"];
+					$_SESSION["username"] = $username;
+				}
 
-			if (empty($_POST["email"])) {
-				$emailErr = "*Email is required";
-				
-			} elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-				$emailErr = "*Invalid email format";
-				
-			}else{
-				$email = $_POST["email"];
-				$_SESSION["email"] = $email;
+				// Email
+				if (empty($_POST["email"])) {
+					$emailErr = "*Email is required";
+				} else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+					$emailErr = "*Invalid email format";
+				} else {
+					$email = $_POST["email"];
+					$_SESSION["email"] = $email;
+				}
 
-			}
-			
-			
-			if (empty($_POST["password"])) {
-               $password = "*password is required";
-			  
-            }elseif (!preg_match("/^[A-Za-z_0-9$][A-Za-z0-9_#*$]*$/",$_POST["password"])) {
+				// Password
+				if (empty($_POST["password"])) {
+					$password = "*password is required";
+				} else if (!preg_match("/^[A-Za-z_0-9$][A-Za-z0-9_#*$]*$/",$_POST["password"])) {
 					$passwordErr = "*Only letters,numbers and underscore are allowed.";
-					
-			} else{
-				
-				 	$password = $_POST["password"];
+				} else {
+					$password = $_POST["password"];
 					$_SESSION["password"] = $password;
+				}
 
-			      }
-
-			
-			
-			if (empty($usernameErr) && empty($emailErr) && empty($passwordErr)) {
-				// No errors, proceed with signup process
-				header("Location: group.php");
-				exit;
+				// Check for blank entries
+				if (empty($usernameErr) && empty($emailErr) && empty($passwordErr)) {
+					// No errors, proceed with signup process
+					header("Location: group.php");
+					exit;
+				}
 			}
-			 
-			
-        }
-	?>
-	 <div class="pic_header"> <img src="./img/jlogo.jpg"> </img>  </div>
-	 <div class="main_class"> 
-     <div class="center_header">
-	 <div class="signup_header"> 
-		<p> Sign up <p>
-	</div>
-	</div>
-	<form class="form" method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> ">
-	
-	<div class = "signup_fields">
-	<div class ="usr_n_span">
-	  <label> Username 
-      <input type="text" name="username" required>
-	  </label>
-	 <span class= "error">  <?php echo $usernameErr; ?> </span> <br>
-	</div>
+		?>
+		<div class="pic_header"> <img src="./img/jlogo.jpg"> </img>  </div>
+		<div class="main_class"> 
+			<div class="center_header">
+				<div class="signup_header"> 
+					<p> Sign up <p>
+				</div>
+			</div>
 
-	<div class ="eml_n_span">
-	<label > Email  
-	<input type="text" name="email" > 
-	</label>
-	<span class= "error">  <?php echo $emailErr; ?> </span> <br>
-	</div>
+			<form class="form" method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> ">
+				<div class = "signup_fields">
+					<div class ="usr_n_span">
+						<label> Username 
+							<input type="text" name="username" required>
+						</label>
+						<span class= "error">  <?php echo $usernameErr; ?> </span> <br>
+					</div>
 
-	<div class ="pass_n_span">	
-	<label > Password  
-		 <input type="password" name="password"  required>
-	 </label>
-	  <span class= "error">  <?php echo $passwordErr; ?> </span> <br>
-	</div>
+					<div class ="eml_n_span">
+						<label > Email  
+							<input type="text" name="email" > 
+						</label>
+						<span class= "error">  <?php echo $emailErr; ?> </span> <br>
+					</div>
 
-	<div class= "submit_align">
-	 <input type= "submit"><br>
-     </div>
-
-	 </div>	
-
-	</form>
-
-	</div>
-	 
-</body>
+					<div class ="pass_n_span">	
+						<label > Password  
+							<input type="password" name="password"  required>
+						</label>
+						<span class= "error">  <?php echo $passwordErr; ?> </span> <br>
+					</div>
+					<div class= "submit_align">
+						<input type= "submit"><br>
+					</div>
+				</div>	
+			</form>
+		</div>
+	</body>
 </html>
-
